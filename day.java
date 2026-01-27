11e8757b3d8aa42cbc637a62b66d087745c37150
Sushant Jadhav <sushant.jadhav@rajasoftwarelabs.com>
2026-01-27 11:41:20 +0530

    added basic class code

--- day.java
+++ day.java
@@ -1,3 +1,19 @@
-public class day-6 {
-    public static void main 
+interface A {
+    void code ();
+    void run2();
+}
+public class day implements  A{
+
+    public void code (){
+        System.out.println("hello this is sushant");
+    }
+    public void run2 (){
+        System.out.println("form the run ");
+    }
+
+    public static void main (){
+        day  a = new day () ;
+        a.code();
+        a.run2 ();
+    }
 }
